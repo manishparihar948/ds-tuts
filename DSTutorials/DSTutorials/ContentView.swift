a870774cdbadb6c4b5e1c90f57d2b144766d6b04
manishparihar948 <manish.parihar948@gmail.com>
2023-12-09 17:25:01 +0100

    Initial project setup

---
+++ DSTutorials/DSTutorials/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DSTutorials
+//
+//  Created by Manish Parihar on 09.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DSTutorials/DSTutorials/DSTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSTutorialsApp.swift
+//  DSTutorials
+//
+//  Created by Manish Parihar on 09.12.23.
+//
+
+import SwiftUI
+
+@main
+struct DSTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
